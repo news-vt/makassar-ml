f5f01db9749c3b2eab6b1db698180410e96f9e34
Alexander DeRieux <alexander.derieux@gmail.com>
2022-01-25 11:45:47 -0500

    Now downloads Beijing PM2.5 dataset

--- makassar_ml/datasets/beijing_pm25.py
+++ makassar_ml/datasets/beijing_pm25.py
@@ -1,6 +1,12 @@
-
+import functools
+from logging.config import RESET_ERROR
+import os
 import pandas as pd
+import pathlib
+import requests
+import shutil
 import torch
+from tqdm.auto import tqdm
 
 class BeijingPM25Dataset(torch.utils.data.Dataset):
     """Wrapper for Beijing PM2.5 dataset.
@@ -22,6 +28,8 @@ class BeijingPM25Dataset(torch.utils.data.Dataset):
         - `Is`: Cumulated hours of snow
         - `Ir`: Cumulated hours of rain
     """
+    dataset_root = pathlib.Path('beijing_pm2.5')
+    dataset_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00381/PRSA_data_2010.1.1-2014.12.31.csv'
 
     # Dataset columnds.
     features = [
@@ -40,9 +48,46 @@ class BeijingPM25Dataset(torch.utils.data.Dataset):
         'Ir',
         ]
 
-    def __init__(self, path: str):
+    def __init__(self, root: str, download: bool = False):
+        # Join path elements.
+        self.dataset_root = root / self.dataset_root
+
+        if download:
+            self.download(self.dataset_root)
+
+        # Load dataset contents.
+        self.load(self.dataset_root)
+
+    def download(self, root: str):
+
+        # Glean name of file from URL and build path.
+        filename = pathlib.Path(self.dataset_url.rsplit('/', 1)[1])
+        filepath = (root / filename).expanduser().resolve()
+
+        # If file already exists, then do not download.
+        if filepath.exists():
+            return
+
+        # Create root path tree.
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+
+        # Get file from URL and save to disk with progress bar.
+        res = requests.get(self.dataset_url, stream=True, allow_redirects=True)
+        file_size = int(res.headers.get('Content-Length', 0))
+        desc = "(Unknown total file size)" if file_size == 0 else ""
+        res.raw.read = functools.partial(res.raw.read, decode_content=True) # Decompress if necessary.
+        with tqdm.wrapattr(res.raw, 'read', total=file_size, desc=desc) as res_raw:
+            with filepath.open('wb') as f:
+                shutil.copyfileobj(res_raw, f)
+
+    def load(self, root: str):
+
+        # Glean name of file from URL and build path.
+        filename = pathlib.Path(self.dataset_url.rsplit('/', 1)[1])
+        filepath = (root / filename).expanduser().resolve()
+
         # Read the input file.
-        self.df = pd.read_csv(path, usecols=self.features)
+        self.df = pd.read_csv(filepath, usecols=self.features)
 
     def __len__(self):
         return self.df.shape[0]
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,4 +1,5 @@
 import csv
+from turtle import down
 import makassar_ml as ml
 import os
 import pytest
@@ -10,8 +11,9 @@ def dataset_root():
 
 
 def test_beijing_pm25(dataset_root):
-    csvfile = os.path.join(dataset_root, "beijing_pm2.5", "PRSA_data_2010.1.1-2014.12.31.csv")
-    dataset = ml.datasets.beijing_pm25.BeijingPM25Dataset(path=csvfile)
+    dataset = ml.datasets.beijing_pm25.BeijingPM25Dataset(root=dataset_root)
+    assert os.path.exists(dataset.dataset_root)
+    assert dataset.df is not None
     x = dataset[[3,4,5]]
     assert isinstance(x, dict)
     assert set(x.keys()) == set(dataset.features)
