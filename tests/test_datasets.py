dbd151d2c8e18c0480f66fdc1151158841943131
Alexander DeRieux <alexander.derieux@gmail.com>
2022-01-28 18:16:08 -0500

    Tests timeseries forecast wrapper

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -3,6 +3,7 @@ from turtle import down
 import makassar_ml as ml
 import os
 import pytest
+import torch
 
 
 @pytest.fixture
@@ -16,3 +17,21 @@ def test_beijing_pm25(dataset_root):
     test = ml.datasets.BeijingPM25Dataset(root=dataset_root, train=False, split=0.2)
     assert os.path.exists(all.dataset_root)
     assert (len(train) + len(test)) == len(all)
+
+
+def test_timeseries_forecast_wrapper(dataset_root):
+    feature_cols = [0,1,2,3]
+    target_cols = [-3]
+    history = 5
+    horizon = 3
+    dset = ml.datasets.BeijingPM25Dataset(root=dataset_root)
+    wrap = ml.datasets.TimeseriesForecastDatasetWrapper(dset, feature_cols, target_cols, history, horizon)
+
+    def verify(window_idx: int):
+        raw_idx = wrap.window2index(window_idx)
+        ctx, tgt = wrap[window_idx]
+        assert torch.equal(ctx, dset[raw_idx:raw_idx+history][:,feature_cols])
+        assert torch.equal(tgt, dset[raw_idx+history:raw_idx+history+horizon][:,target_cols])
+
+    verify(0)
+    verify(-1)
