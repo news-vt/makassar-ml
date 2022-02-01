c9a8a70ff31e4f0c944856ed87c7e403f4773856
Alexander DeRieux <alexander.derieux@gmail.com>
2022-01-31 23:46:49 -0500

    Imports future annotations

--- makassar_ml/datasets/beijing_pm25.py
+++ makassar_ml/datasets/beijing_pm25.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from .csv_timeseries import CsvTimeseriesDataset
 import functools
 import pandas as pd
--- makassar_ml/datasets/timeseries_forecast_wrapper.py
+++ makassar_ml/datasets/timeseries_forecast_wrapper.py
@@ -1,4 +1,4 @@
-
+from __future__ import annotations
 from .csv_timeseries import CsvTimeseriesDataset
 import torch
 import numpy as np
