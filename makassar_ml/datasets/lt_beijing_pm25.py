6c42a9aba2e369dc615e6bef9ad6e8b5bea722b4
Alexander DeRieux <alexander.derieux@gmail.com>
2022-02-08 17:42:08 -0500

    First draft of PyTorch Lightning data module for Beijing PM2.5 dataset

---
+++ makassar_ml/datasets/lt_beijing_pm25.py
@@ -0,0 +1,21 @@
+import pytorch_lightning as pl
+import torch
+
+
+class BeijingPM25LightningDataModule(pl.LightningDataModule):
+    def __init__(self, 
+        root: str, 
+        feature_cols: list[int], 
+        target_cols: list[int], 
+        history: int, 
+        horizon: int, 
+        split: float,
+        batch_size: int,
+        ):
+        self.root = root
+        self.feature_cols = feature_cols
+        self.target_cols = target_cols
+        self.history = history
+        self.horizon = horizon
+        self.split = split
+        self.batch_size = batch_size
