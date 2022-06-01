7c6bf98cb381199a8837595775f2aa0bcac50b47
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-01 14:55:47 -0400

    wip: adding `train_evaluate_for_dataset`

--- makassar_ml/training.py
+++ makassar_ml/training.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 import inspect
 import json
+import logging
+from typing import Callable
 import pandas as pd
+from pathlib import Path
 import tensorflow as tf
 import tensorflow.keras as keras
+
+# Create logger for module.
+logger = logging.getLogger(__name__)
 
 
 def load_metrics(metrics_path: str):
@@ -134,3 +140,156 @@ def train_evaluate_model(
         json.dump(metrics, f)
 
     return model, history.history, metrics
+
+
+def ensure_path(s: None|str|Path, default: str|Path = '.') -> Path:
+    if s is None:
+        s = Path(default)
+    elif not isinstance(s, Path):
+        s = Path(s)
+    return s
+
+
+def train_evaluate_for_dataset(
+    model_name: str,
+    build_model_func: Callable[[], keras.Model],
+    dataset_loader_func: Callable[[int], tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]],
+    # params: dict,
+    # compile_params: dict,
+    # in_seq_len: int,
+    # out_seq_len: int,
+    # shift: int,
+    # split: tuple[float, float, float],
+    # in_feat: list[str],
+    # out_feat: list[str],
+    metric_list: list[str],
+    batch_size: int = 128,
+    # shuffle: bool = False,
+    strategy: tf.distribute.Strategy = tf.distribute.get_strategy(),
+    epochs: int = 10,
+    table_header: list = None,
+    table_omit_cols: str|list[str] = None,
+    checkpoint_root: str = None,
+    image_root: str = None,
+    table_root: str = None,
+    ) -> tuple[keras.models.Model, dict, dict, dict, pd.DataFrame]:
+    """Train and evaluate a model on a given dataset.
+
+    If checkpoint exists then the model is loaded in place of training.
+    """
+    # Ensure roots are path objects.
+    checkpoint_root = ensure_path(checkpoint_root)
+    image_root = ensure_path(image_root)
+    table_root = ensure_path(table_root)
+
+    # Train and evaluate model.
+    checkpoint_path = checkpoint_root/model_name/'model.h5'
+    history_path = checkpoint_path.parent/'history.csv'
+    metrics_path = checkpoint_path.parent/'metrics.json'
+
+    # Maximize batch size efficiency using distributed strategy.
+    batch_size_per_replica = batch_size
+    batch_size = batch_size_per_replica * strategy.num_replicas_in_sync
+
+    # Load model from best checkpoint.
+    if checkpoint_path.exists() and history_path.exists() and metrics_path.exists():
+        # Load the model.
+        logger.info(f"[{model_name}] Loading best model from: {checkpoint_path}")
+        with strategy.scope():
+            model = keras.models.load_model(checkpoint_path, custom_objects=keras.utils.get_custom_objects())
+
+        # Load history and metrics.
+        logger.info(f"[{model_name}] Loading from save data")
+        hist = load_history(history_path)
+        met = load_metrics(metrics_path)
+
+    # Train model.
+    else:
+        logger.info(f"[{model_name}] Training new model: {epochs=}, {batch_size=}, {strategy=}")
+
+        # Use strategy memory.
+        with strategy.scope():
+
+            # Load the dataset.
+            dataset_train, dataset_val, dataset_test = dataset_loader_func(batch_size=batch_size)
+
+            # Create and compile model.
+            model = build_model_func()
+
+        # Train the model using the strategy.
+        model, hist, met = train_evaluate_model(
+            model,
+            datagen_train=dataset_train,
+            datagen_val=dataset_val,
+            datagen_test=dataset_test,
+            epochs=epochs,
+            metric_list=metric_list,
+            checkpoint_path=checkpoint_path,
+            history_path=history_path,
+            metrics_path=metrics_path,
+        )
+
+    return model, hist, met
+
+
+    # # Plot the model predictions of the dataset.
+    # figs: dict = plot_predictions(
+    #     model=model,
+    #     in_seq_len=in_seq_len,
+    #     out_seq_len=out_seq_len,
+    #     shift=shift,
+    #     split=split,
+    #     in_feat=in_feat,
+    #     out_feat=out_feat,
+    #     batch_size=batch_size,
+    #     shuffle=shuffle,
+    #     strategy=strategy,
+    # )
+    # for name, fig in figs.items():
+    #     fig.savefig(image_root/f"{model_name}_io_{name}.png", bbox_inches='tight')
+    #     fig.show()
+
+    # # Print model summary.
+    # logger.info(f"[{model_name}] Model Summary:")
+    # model.summary(print_fn=logger.info)
+
+    # logger.info(f"[{model_name}] Training and Evaluation Results:")
+
+    # # Build dataframe using results.
+    # df = pd.DataFrame([{
+    #     'model': model_name,
+    #     **met,
+    #     **params,
+    # }])
+    # if table_header is not None:
+    #     df = df[table_header]
+    # df = df.drop(columns=table_omit_cols, errors='ignore')
+    # logger.info(df.to_string(index=False)) # Log to console.
+
+    # # Export the dataframe to LaTeX using custom style.
+    # df = df.drop(columns=table_omit_cols, errors='ignore')
+    # df.columns = df.columns.map(lambda x: x.replace('_', '\_')) # Escape the header names too.
+    # styler = df.style
+    # styler = styler.format(str, escape='latex') # Default is to convert all cells to their string representation.
+    # subset = []
+    # for m in compile_params['metrics']+['loss']:
+    #     if m in set(df.columns):
+    #         subset.append(f"{m}")
+    #         subset.append(f"val\_{m}")
+    #         subset.append(f"test\_{m}")
+    # styler = styler.format(formatter='{:.4f}', subset=subset)
+    # styler = styler.highlight_min(subset=subset, axis=0, props='textbf:--rwrap;')
+    # styler = styler.hide(axis=0) # Hide the index.
+    # styler.to_latex(
+    #     buf=table_root/f"{model_name}_results.tex",
+    #     hrules=True,
+    # )
+    # df.columns = df.columns.map(lambda x: x.replace('\_', '_')) # Convert header names back.
+
+    # # Plot train/val performance.
+    # for key in compile_params['metrics']+['loss']:
+    #     fig = plot_metric(hist, key)
+    #     fig.savefig(image_root/f"{model_name}_{key}.png", bbox_inches='tight')
+    #     fig.show()
+
+    # return model, hist, met, params, df
