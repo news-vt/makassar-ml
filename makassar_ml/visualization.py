94a667ffd7a577332996dc1b5a55a02add995514
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-01 12:10:27 -0400

    Add `plot_input_output`

--- makassar_ml/visualization.py
+++ makassar_ml/visualization.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
+import tensorflow as tf
 
 
 def plot_metric(history: dict, metric: str) -> plt.Figure:
@@ -11,4 +14,63 @@ def plot_metric(history: dict, metric: str) -> plt.Figure:
     plt.xlabel('Epoch')
     plt.ylabel(metric.upper())
     plt.legend(loc='upper left')
+    return fig
+
+
+def plot_input_output(
+    df: pd.DataFrame,
+    pred: tf.Tensor,
+    in_seq_len: int,
+    out_seq_len: int,
+    shift: int,
+    in_feat: list[str],
+    out_feat: list[str],
+    x_key: str,
+    figsize: tuple = (15,7),
+    width_ratios: list = [1, 1.75],
+    wspace: float = 0.07,
+    xtick_rotation: float = 45,
+    ) -> plt.Figure:
+    """Creates figure for input/output feature data."""
+    fig = plt.figure(constrained_layout=True, figsize=figsize)
+    subfigs = fig.subfigures(nrows=1, ncols=2, wspace=wspace, width_ratios=width_ratios)
+
+    # Plot all input features.
+    subfigs[0].suptitle('Input Features', fontsize='x-large')
+    axs = subfigs[0].subplots(nrows=len(in_feat), ncols=1, sharex=True, squeeze=False)
+    for r, key in enumerate(in_feat):
+        sns.lineplot(
+            data=df,
+            x=x_key,
+            y=key,
+            ax=axs[r,0],
+        )
+    plt.xticks(rotation=xtick_rotation)
+
+    # Plot all output features against truth.
+    subfigs[1].suptitle('Output Features', fontsize='x-large')
+    axs = subfigs[1].subplots(nrows=len(out_feat), ncols=1, sharex=True, squeeze=False)
+    for r, key in enumerate(out_feat):
+        sns.lineplot(
+            data=df,
+            x=x_key,
+            y=key,
+            ax=axs[r,0],
+            label='truth',
+        )
+        axs[r,0].plot(
+            df.iloc[
+                np.arange(
+                    start=in_seq_len,
+                    stop=pred.shape[0]+in_seq_len,
+                    step=shift,
+                )
+            ][x_key],
+            pred[:,r],
+            linewidth=3,
+            label=f'forecast',
+        )
+        axs[r,0].legend(loc='upper right')
+    plt.xticks(rotation=xtick_rotation)
+
     return fig
