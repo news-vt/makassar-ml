db8f3150fcc8bd1079db9352569e320b69b3c4b3
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-07 10:15:08 -0400

    refactor: Now uses YAML config
    - Program arguments come from YAML config
      - Model info
      - Optimizer
      - Compiler
      - Train settings
      - Dataset info
      - Root directory paths
    - YAML supports environment vars

--- scripts/train_beijingpm25.py
+++ scripts/train_beijingpm25.py
@@ -10,31 +10,19 @@ path = fdir/'..'
 if path not in sys.path:
     sys.path.append(str(path))
 
-# Load config file if available.
-import yaml
-config_path = fdir/'config.yml'
-if config_path.exists():
-    config = yaml.safe_load(open(config_path))
-
-# Process configs.
-for key in config['roots']:
-    # Convert to path.
-    config['roots'][key] = Path(config['roots'][key]).expanduser()
-    # Create path if necessary.
-    config['roots'][key].mkdir(parents=True, exist_ok=True)
-
 # Complete imports.
 import argparse
-import inspect
+# import inspect
 import logging
 import makassar_ml as ml
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# import seaborn as sns
 import tensorflow as tf
 import tensorflow.keras as keras
 from tqdm.keras import TqdmCallback
+import yaml
 
 # Set random seeds.
 SEED = 0
@@ -65,146 +53,75 @@ for gpu in gpus:
 
 def get_opts() -> argparse.Namespace:
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('model',
+    parser.add_argument('config',
         type=str,
-        choices=[tup[0] for tup in inspect.getmembers(ml.models, inspect.ismodule)],
-        help='Model name to train.'
-    )
-    parser.add_argument('--lr',
-        type=float,
-        required=True,
-        help='Learning rate',
-    )
-    parser.add_argument('--epochs', 
-        type=int,
-        required=True,
-        help='Number of training epochs.',
-    )
-    parser.add_argument('--in_seq_len', 
-        type=int,
-        required=True,
-        help='Input sequence length.',
-    )
-    parser.add_argument('--out_seq_len', 
-        type=int,
-        required=True,
-        help='Output sequence length.',
-    )
-    parser.add_argument('--shift', 
-        type=int,
-        required=True,
-        help='Number of records to skip between data sequences.',
-    )
-    parser.add_argument('--split',
-        type=float,
-        nargs=3,
-        default=(0.7,0.2,0.1),
-        help='Data train/val/test split.',
-    )
-    parser.add_argument('--in_feat',
-        type=str,
-        nargs='+',
-        default=['TEMP','PRES','Iws','Is','Ir'],
-        help='Input feature list.',
-    )
-    parser.add_argument('--out_feat',
-        type=str,
-        nargs='+',
-        default=['DEWP'],
-        help='Output feature list.',
-    )
-    parser.add_argument('--batch_size',
-        type=int,
-        default=128,
-        help='Batch size.',
-    )
-    parser.add_argument('--shuffle',
-        action='store_true',
-        help='Shuffle the dataset.',
+        help='Configuration file.'
     )
     opts = parser.parse_args()
     return opts
 
 
-def main(opts: argparse.Namespace):
+def main(config: dict):
+
     # Set training strategy.
     strategy = tf.distribute.get_strategy()
 
-    # Define model compilation parameters.
-    # (i.e., loss, metrics, etc.)
-    compile_params = dict(
-        loss='mse',
-        metrics=['mae', 'mape'],
-    )
-
     def build_model_func() -> keras.Model:
         # Get build function for specific model.
-        build_model = getattr(ml.models, opts.model).build_model
+        build_model = getattr(ml.models, config['model']['name']).build_model
         model = build_model(
-            in_seq_len=opts.in_seq_len,
-            in_feat=len(opts.in_feat),
-            out_feat=len(opts.out_feat),
-
-            fc_units=[64,64],
-            embed_dim=64,
-            n_heads=8,
-            ff_dim=256,
-            dropout=0.1,
-            n_encoders=3,
+            **config['model']['params']
         )
 
         # Configure optimizer.
         # optim = keras.optimizers.Adam(learning_rate=lr_schedule)
-        optim = keras.optimizers.Adam(learning_rate=opts.lr)
+        optim = keras.optimizers.get({
+        'class_name': config['model']['optimizer']['name'],
+            'config': config['model']['optimizer']['params'],
+        })
 
         # Compile the model.
         model.compile(
             optimizer=optim,
-            loss=compile_params['loss'],
-            metrics=compile_params['metrics'],
+            **config['model']['compile'],
         )
         return model
 
     def dataset_loader_func(batch_size: int) -> tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]:
         return ml.datasets.beijingpm25.load_beijingpm25_ds(
-            in_seq_len=opts.in_seq_len,
-            out_seq_len=opts.out_seq_len,
-            shift=opts.shift,
-            in_feat=opts.in_feat,
-            out_feat=opts.out_feat,
-            split=opts.split,
-            shuffle=opts.shuffle,
-            path=config['roots']['dataset_root']/'beijing_pm25',
+            **config['dataset']['params'],
             batch_size=batch_size,
         )
 
     # Train and evaluate the model.
     model, hist, met = ml.training.train_evaluate_for_dataset(
-        model_name=opts.model,
+        model_name=config['model']['name'],
         build_model_func=build_model_func,
         dataset_loader_func=dataset_loader_func,
-        metric_list=compile_params['metrics'],
-        batch_size=opts.batch_size,
+        metric_list=config['model']['compile']['metrics'],
+        batch_size=config['train']['batch_size'],
         strategy=strategy,
-        epochs=opts.epochs,
+        epochs=config['train']['epochs'],
         checkpoint_root=config['roots']['checkpoint_root'],
         callbacks=[TqdmCallback(verbose=2)]
     )
     model.summary(print_fn=logger.info)
 
     # Plot train/val performance.
-    for key in compile_params['metrics']+['loss']:
+    for key in config['model']['compile']['metrics']+['loss']:
         fig = ml.visualization.plot_metric(hist, key)
-        fig.savefig(config['roots']['image_root']/f"{opts.model}_metric_{key}.png", bbox_inches='tight')
+        path = Path(config['roots']['image_root'])/f"{config['model']['name']}_metric_{key}.png"
+        fig.savefig(path, bbox_inches='tight')
+        logger.info(f"{path}")
         # fig.show()
 
     # Load the data in dataframe form.
     df_train, df_val, df_test = ml.datasets.beijingpm25.load_beijingpm25_df(
-        split=opts.split,
-        path=config['roots']['dataset_root']/'beijing_pm25',
+        split=config['dataset']['params']['split'],
+        path=config['dataset']['params']['path'],
     )
     # Load the data in dataset form.
-    dataset_train, dataset_val, dataset_test = dataset_loader_func(opts.batch_size)
+    dataset_train, dataset_val, dataset_test = dataset_loader_func(config['train']['batch_size'])
     # Data keys.
     keys = [
         'year',
@@ -237,38 +154,31 @@ def main(opts: argparse.Namespace):
         fig = ml.visualization.plot_input_output(
             df=locals()[f"df_{label}"],
             pred=locals()[f"{label}_pred"],
-            in_seq_len=opts.in_seq_len,
-            out_seq_len=opts.out_seq_len,
-            shift=opts.shift,
-            in_feat=opts.in_feat,
-            out_feat=opts.out_feat,
+            in_seq_len=config['dataset']['params']['in_seq_len'],
+            out_seq_len=config['dataset']['params']['out_seq_len'],
+            shift=config['dataset']['params']['shift'],
+            in_feat=config['dataset']['params']['in_feat'],
+            out_feat=config['dataset']['params']['out_feat'],
             x_key='datetime',
         )
         # fig.suptitle(f"{label[0].upper()}{label[1:]} Data", fontsize='xx-large')
         figs[label] = fig
     for name, fig in figs.items():
-        fig.savefig(config['roots']['image_root']/f"{opts.model}_io_{name}.png", bbox_inches='tight')
+        path = Path(config['roots']['image_root'])/f"{config['model']['name']}_io_{name}.png"
+        fig.savefig(path, bbox_inches='tight')
+        logger.info(f"{path}")
         # fig.show()
 
 
 if __name__ == '__main__':
+    # Get program options.
     opts = get_opts()
-    main(opts)
 
-# # Setup paths.
-# DATASET_ROOT = Path('~/research/makassar/datasets').expanduser()
-# if not DATASET_ROOT.exists(): raise ValueError(f"Dataset root directory does not exist at {DATASET_ROOT}")
-# PROJECT_ROOT = Path('~/research/makassar').expanduser()
-# CHECKPOINT_ROOT = PROJECT_ROOT / 'checkpoints'
-# IMAGE_ROOT = PROJECT_ROOT / 'images'
-# TABLE_ROOT = PROJECT_ROOT / 'tables'
-# HP_TUNING_ROOT = PROJECT_ROOT / 'hp_tuning'
-# KERAS_TUNER_PATH = PROJECT_ROOT / 'keras_tuner'
+    # Load YAML config.
+    config_path = Path(opts.config)
+    if config_path.exists():
+        with open(config_path) as f:
+            config = yaml.load(f, Loader=ml.yaml.EnvVarLoader)
 
-# # Ensure some directories exist.
-# PROJECT_ROOT.mkdir(parents=True, exist_ok=True)
-# CHECKPOINT_ROOT.mkdir(parents=True, exist_ok=True)
-# IMAGE_ROOT.mkdir(parents=True, exist_ok=True)
-# TABLE_ROOT.mkdir(parents=True, exist_ok=True)
-# HP_TUNING_ROOT.mkdir(parents=True, exist_ok=True)
-# KERAS_TUNER_PATH.mkdir(parents=True, exist_ok=True)
+    # Run main training function.
+    main(config)
