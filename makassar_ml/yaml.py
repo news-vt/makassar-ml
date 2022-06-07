904ef3245252b40d2ae71910cbe9eb368f8eed24
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-07 18:09:10 -0400

    refactor: Add support for `!join` tag
    - Renamed `EnvVarLoader` to `ConfigLoader`.
    - Now supports `!join` tag to concatenate strings.

--- makassar_ml/yaml.py
+++ makassar_ml/yaml.py
@@ -7,8 +7,14 @@ path_matcher = re.compile(r'.*(\$\{([^}^{]+)\})|(\$([^}^{]+)).*')
 def path_constructor(loader, node):
     return os.path.expandvars(node.value)
 
-# Loader for environment variables within YAML files.
-class EnvVarLoader(yaml.SafeLoader):
+# Concatenate strings.
+def join_constructor(loader, node):
+    seq = loader.construct_sequence(node)
+    return ''.join([str(item) for item in seq])
+
+# Configuration loader with custom tags.
+class ConfigLoader(yaml.SafeLoader):
     pass
-EnvVarLoader.add_implicit_resolver('!path', path_matcher, None)
-EnvVarLoader.add_constructor('!path', path_constructor)
+ConfigLoader.add_implicit_resolver('!path', path_matcher, None)
+ConfigLoader.add_constructor('!path', path_constructor)
+ConfigLoader.add_constructor('!join', join_constructor)
