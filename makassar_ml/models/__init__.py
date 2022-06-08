5b85a3c036ccd8004b36434fd655fc915fbd25f9
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-08 18:56:26 -0400

    New model `transformer_time2vec_linearencoding_encoder_flatten`
    Layers are:
    - `Time2Vec`
    - `LinearEncoding2D`
    - `TransformerEncoderLayer`
    - `Flatten`
    - Linear

--- makassar_ml/models/__init__.py
+++ makassar_ml/models/__init__.py
@@ -2,5 +2,6 @@ from . import (
     lstm_net,
     transformer_time2vec_encoder_pool_fc,
     transformer_time2vec_encoder_flatten_fc,
+    transformer_time2vec_linearencoding_encoder_flatten,
     transformer_linearembedding_linearencoding_encoder_flatten_fc,
 )
---
+++ makassar_ml/models/transformer_time2vec_linearencoding_encoder_flatten.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+import tensorflow as tf
+import tensorflow.keras as keras
+from ..layers import Time2Vec, TransformerEncoderLayer, LinearEmbedding2D, LinearEncoding2D
+
+
+def build_model(
+    in_seq_len: int,
+    in_feat: int,
+    out_feat: int,
+    embed_dim: int,
+    n_heads: int,
+    key_dim: int = None,
+    value_dim: int = None,
+    ff_dim: int = 2048,
+    dropout: float = 0.0,
+    n_encoders: int = 3,
+    ) -> keras.Model:
+
+    # Input sequence of features.
+    inp = keras.Input(shape=(in_seq_len, in_feat))
+    # Create common model input/output variable.
+    x = inp
+    # Time embedding.
+    x = Time2Vec(embed_dim=embed_dim)(x)
+    # Linear positional encoding.
+    x = LinearEncoding2D()(x)
+    # Pass combined featured through cascaded self-attention encoder sublayers.
+    for _ in range(n_encoders):
+        x = TransformerEncoderLayer(
+            model_dim=embed_dim,
+            key_dim=key_dim,
+            n_heads=n_heads,
+            ff_dim=ff_dim,
+            value_dim=value_dim,
+            dropout=dropout,
+        )(x)
+    x = keras.layers.Flatten(data_format='channels_last')(x) # Flatten to 1D (in_seq_len*embed_dim,)
+    # Classifier.
+    x = keras.layers.Dense(units=out_feat, activation='linear')(x)
+
+    # Construct model class and return.
+    return keras.Model(inputs=inp, outputs=x)
