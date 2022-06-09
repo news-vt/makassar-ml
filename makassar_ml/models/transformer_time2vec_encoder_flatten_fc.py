73ffa13e4c40e64074af27fc0f55bc52d3c8be3b
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-09 19:30:18 -0400

    refactor: Now complies with latest Time2Vec dimensions

--- makassar_ml/models/transformer_time2vec_encoder_flatten_fc.py
+++ makassar_ml/models/transformer_time2vec_encoder_flatten_fc.py
@@ -20,19 +20,25 @@ def build_model(
 
     # Input sequence of features.
     inp = keras.Input(shape=(in_seq_len, in_feat))
+    # Create common model input/output variable.
+    x = inp
     # Time embedding.
-    x = Time2Vec(embed_dim=embed_dim)(inp)
+    x = Time2Vec(embed_dim=embed_dim)(x)
+    # Combine input with embedding to form attention input features.
+    x = keras.layers.Concatenate(axis=-1)([inp, x])
+    # Compute model dimension for Transformer encoder.
+    model_dim: int = x.shape[-1]
     # Pass combined featured through cascaded self-attention encoder sublayers.
     for _ in range(n_encoders):
         x = TransformerEncoderLayer(
-            model_dim=embed_dim,
+            model_dim=model_dim,
             key_dim=key_dim,
             n_heads=n_heads,
             ff_dim=ff_dim,
             value_dim=value_dim,
             dropout=dropout,
         )(x)
-    x = keras.layers.Flatten(data_format='channels_last')(x) # Flatten to 1D (in_seq_len*embed_dim,)
+    x = keras.layers.Flatten(data_format='channels_last')(x) # Flatten to 1D (in_seq_len*model_dim,)
     # Fully-connected network before classifier.
     for units in fc_units: 
         x = keras.layers.Dense(units=units, activation='relu')(x)
--- makassar_ml/models/transformer_time2vec_linearencoding_encoder_flatten.py
+++ makassar_ml/models/transformer_time2vec_linearencoding_encoder_flatten.py
@@ -25,17 +25,21 @@ def build_model(
     x = Time2Vec(embed_dim=embed_dim)(x)
     # Linear positional encoding.
     x = LinearEncoding2D()(x)
+    # Combine input with embedding to form attention input features.
+    x = keras.layers.Concatenate(axis=-1)([inp, x])
+    # Compute model dimension for Transformer encoder.
+    model_dim: int = x.shape[-1]
     # Pass combined featured through cascaded self-attention encoder sublayers.
     for _ in range(n_encoders):
         x = TransformerEncoderLayer(
-            model_dim=embed_dim,
+            model_dim=model_dim,
             key_dim=key_dim,
             n_heads=n_heads,
             ff_dim=ff_dim,
             value_dim=value_dim,
             dropout=dropout,
         )(x)
-    x = keras.layers.Flatten(data_format='channels_last')(x) # Flatten to 1D (in_seq_len*embed_dim,)
+    x = keras.layers.Flatten(data_format='channels_last')(x) # Flatten to 1D (in_seq_len*model_dim,)
     # Classifier.
     x = keras.layers.Dense(units=out_feat, activation='linear')(x)
 
