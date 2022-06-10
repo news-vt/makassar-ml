8e87783d982d685be7cd9573d5e90f515a26f169
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-10 10:14:20 -0400

    Imports are now lexically sorted

--- makassar_ml/models/__init__.py
+++ makassar_ml/models/__init__.py
@@ -1,7 +1,7 @@
 from . import (
     lstm_net,
+    transformer_linearembedding_linearencoding_encoder_flatten_fc,
     transformer_time2vec_encoder_pool_fc,
     transformer_time2vec_encoder_flatten_fc,
     transformer_time2vec_linearencoding_encoder_flatten,
-    transformer_linearembedding_linearencoding_encoder_flatten_fc,
 )
