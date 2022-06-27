462f9008de42d3741a2af4fd12ea7a27dd3d1be2
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-27 18:45:26 -0400

    Add `Patches` and `PatchEncoder`

--- makassar_ml/layers/__init__.py
+++ makassar_ml/layers/__init__.py
@@ -2,3 +2,5 @@ from .time2vec import Time2Vec
 from .transformer import PointWiseFeedForwardLayer, TransformerEncoderLayer, TransformerDecoderLayer
 from .linear_embedding_2d import LinearEmbedding2D
 from .linear_encoding_2d import LinearEncoding2D
+from .patches import Patches
+from .patch_encoder import PatchEncoder
