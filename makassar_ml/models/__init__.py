a0d0f414cbad1d0f1331c1ac2738fab0fad27ba9
Alexander DeRieux <alexander.derieux@gmail.com>
2022-07-03 11:19:01 -0400

    New fusion model for image+timeseries multi-task learning
    - Classification
    - Regression

--- makassar_ml/models/__init__.py
+++ makassar_ml/models/__init__.py
@@ -16,4 +16,5 @@ from .fut import (
     FuT,
     FuT_image_timeseries_classifier,
     FuT_image_timeseries_regression,
+    FuT_image_timeseries_multitask,
 )
--- makassar_ml/models/fut.py
+++ makassar_ml/models/fut.py
@@ -12,7 +12,7 @@ from ..layers import (
 
 def FuT(
     inputs: list[keras.Model|tf.Tensor],
-    task_head: keras.layers.Layer,
+    task_head: keras.layers.Layer|list[keras.layers.Layer],
     n_heads: int = 8,
     key_dim: int = None,
     value_dim: int = None,
@@ -64,10 +64,17 @@ def FuT(
         io_outputs.append(x)
 
     # Pass branch outputs to final task head.
-    o = task_head(io_outputs)
+    if isinstance(task_head, keras.layers.Layer):
+        task_heads = [task_head]
+    else:
+        task_heads = task_head
+    outputs = []
+    for head in task_heads:
+        o = head(io_outputs)
+        outputs.append(o)
 
     # Construct model class and return.
-    return keras.Model(inputs=io_inputs, outputs=o)
+    return keras.Model(inputs=io_inputs, outputs=outputs)
 
 
 
@@ -174,6 +181,72 @@ def FuT_image_timeseries_regression(
     model = FuT(
         inputs=inputs,
         task_head=task_head,
+        n_heads=n_heads,
+        key_dim=key_dim,
+        value_dim=value_dim,
+        ff_dim=ff_dim,
+        dropout=dropout,
+        n_encoders=n_encoders,
+    )
+    return model
+
+
+def FuT_image_timeseries_multitask(
+    image_shape: tuple,
+    in_seq_shape: tuple,
+    out_seq_shape: tuple,
+    n_class: int,
+    patch_size: int,
+    image_embed_dim: int,
+    seq_embed_dim: int,
+    fusion_embed_dim: int,
+    num_patches: int = None,
+    n_heads: int = 8,
+    key_dim: int = None,
+    value_dim: int = None,
+    ff_dim: int = 2048,
+    fc_units: list[int] = [],
+    dropout: float = 0.0,
+    n_encoders: int = 3,
+    ) -> keras.Model:
+    """Vision Forecast Transformer for regression tasks."""
+    
+    if num_patches is None:
+        num_patches = (image_shape[0]//patch_size)**2
+
+    # Input heads.
+    inputs = [
+        ImageInputHead(
+            shape=image_shape,
+            patch_size=patch_size,
+            num_patches=num_patches,
+            embed_dim=image_embed_dim,
+        ),
+        TimeSeriesInputHead(
+            shape=in_seq_shape,
+            embed_dim=seq_embed_dim,
+        ),
+    ]
+
+    # Task head.
+    task_heads = [
+        ClassificationTaskHead(
+            n_class=n_class,
+            dropout=dropout,
+            fc_units=fc_units,
+            name='classifier',
+        ),
+        RegressionTaskHead(
+            out_seq_shape=out_seq_shape,
+            embed_dim=fusion_embed_dim,
+            name='regressor',
+        ),
+    ]
+
+    # Create fusion model.
+    model = FuT(
+        inputs=inputs,
+        task_head=task_heads,
         n_heads=n_heads,
         key_dim=key_dim,
         value_dim=value_dim,
